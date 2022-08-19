8476ed8c969cd0b04fbf961157ada40de1006ccb
zzllx1 <zhuxinlei1234@gmail.com>
2022-08-19 08:11:48 +0000

    weed8

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,34 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config()
 
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
 /**
- * @type import('hardhat/config').HardhatUserConfig
+ * 切记私钥不要上传到仓库 切记 切记
  */
 
 module.exports = {
   solidity: "0.8.4",
-
   networks: {
     
     "optimism": {
-       url: process.env.URL,
-       accounts: [ process.env.MNEMONIC ]
+       url: "https://opt-mainnet.g.alchemy.com/v2/fix9KCUH9Mcy35g_If9sYPNQdV7qsNwm",
+       accounts: [ "a80c675b448cee6b58e9e5b2f8a2fbaf50fdf0725006f12a3d685c1888130f34" ]
     }
   }
 };
